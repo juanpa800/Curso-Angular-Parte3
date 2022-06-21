328574278693159092b6cbaa417b9dc91cbd17b1
canojuanpa800 <canojuanpa@gmail.com>
2022-06-20 22:02:09 -0500

    new aplication buil apply course previous

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Curso-Angular-Parte3';
+  titulo = 'Curso Angular Parte 3';
+  message: string = "";
+  userName: string = "";
+  password: string = "";
+  registeredUsers: string[] = []; //declare type of array, default is never
+  passwordList: string[] = [];
+
+
+  LogIn(): string {
+    if (this.passwordList[this.registeredUsers.indexOf(this.userName)] == this.password) {
+      this.message = "Log In complete";
+    }
+    else {
+      this.message = "Incorrect user or password"
+    }
+    return "0";
+  }
+
+  SignIn() {
+    if ((this.registeredUsers).includes(this.userName)) {
+      this.message = "User " + this.userName + " already exist";
+    }
+    else {
+      this.registeredUsers.push(this.userName);
+      this.passwordList.push(this.password);
+      this.message = "User " + this.userName + " Sign in";
+
+    }
+  }
 }
